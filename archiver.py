db49ec4b2bbed5de7a3cdb86c642279cb531faf9
MRtecno98 <mr.tecno98@gmail.com>
2022-09-12 17:19:09 +0200

    Added multithreading and output folder
    Currently being ratelimited by Wattpad when running at full speed,
    output folder works perfectly tho :D

--- archiver.py
+++ archiver.py
@@ -5,6 +5,7 @@ USERNAME = os.environ.get("WATTPAD_USERNAME", "")
 TOKEN = os.environ.get("TOKEN", "")
 AGENT = os.environ.get("AGENT", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/105.0.0.0 Safari/537.36")
 
+MULTITHREAD = os.environ.get("MULTITHREAD", "false").lower() == "true"
 OUTPUT = os.environ.get("OUTPUT", ".")
 
 ENDPOINT = "https://www.wattpad.com/api/v3/users/{username:s}/library"
@@ -20,6 +21,13 @@ LANGUAGES = {
 	1: "en",
 	3: "it",
 }
+
+SYMBOLS = set(r"""`~!@#$%^&*()_-+={[}}|\:;"'<,>.?/""")
+
+if MULTITHREAD:
+	import threading
+
+errors = 0
 
 def get_request(url, stream=None, **kwargs):
 	return requests.get(url, params=kwargs, stream=stream,
@@ -59,6 +67,10 @@ print("Agent\t :\t", AGENT)
 print("Endpoint :\t", ENDPOINT.format(username=USERNAME))
 print()
 
+print("Output\t\t:\t\"" + OUTPUT + "\"")
+print("Multithreading\t:\t" + ("ON" if MULTITHREAD else "OFF"))
+print()
+
 print("Getting library count")
 count = process_request(ENDPOINT.format(username=USERNAME), fields="total")["total"]
 print("Found " + str(count) + " stories in library")
@@ -86,106 +98,140 @@ with open("templates/title.xhtml") as f:
 
 print("Loaded templates\n")
 
+threads = []
+
 i = 0
 for story in result["stories"]:
 	if i >= 3:
 		break
 	i += 1
 	
-	loc = time.time()
-	filename = os.path.join(OUTPUT, story["title"].replace(" ", "_") + ".epub")
+	def process_story(story):
+		global errors
 
-	print("Processing " + story["title"] + " (" + str(story["id"]) + ")")
+		loc = time.time()
+		filename = os.path.join(OUTPUT, "".join(c for c in story["title"] if c not in SYMBOLS).replace(" ", "_") + ".epub")
 
-	book = epub.EpubBook()
-	book.set_identifier("wp" + story["id"])
-	book.set_title(story["title"])
+		print("Processing " + story["title"] + " (" + str(story["id"]) + ")")
 
-	if story["language"]["id"] in LANGUAGES:
-		book.set_language(LANGUAGES[story["language"]["id"]])
-	else:
-		print("Unknown language tag for ID " \
-			+ f"{str(story['language']['id'])} (\"{story['language']['name']}\"), " \
-			+ "defaulting to \"en\"", file=sys.stderr)
-		book.set_language("en")
+		book = epub.EpubBook()
+		book.set_identifier("wp" + story["id"])
+		book.set_title(story["title"])
 
-	book.add_author(f"{story['user']['name']} ({story['user']['username']})")
-	book.set_cover("cover.jpg", get_request(story["cover"]).content)
-
-	# Buttload of metadata ('cause we've got it so why not)
-	book.add_metadata('DC', 'description', story["description"])
-	book.add_metadata('DC', 'publisher', "Wattpad")
-	book.add_metadata('DC', 'date', story["createDate"])
-	book.add_metadata('DC', 'subject', ", ".join(story["tags"]))
-	book.add_metadata('DC', 'type', "Text")
-	book.add_metadata('DC', 'format', "application/epub+zip")
-	book.add_metadata('DC', 'identifier', "wp" + story["id"])
-	book.add_metadata('DC', 'source', story["url"])
-	book.add_metadata('DC', 'language', book.language)
-	book.add_metadata('DC', 'creator', story["user"]["name"])
-	book.add_metadata('DC', 'creator', story["user"]["username"])
-
-	cover = epub.EpubHtml(title="Cover", file_name="cover_page.xhtml", lang=book.language, 
-		content=cover_template.format(title=story["title"]).encode())
-	title = epub.EpubHtml(title="Title Page", file_name="title.xhtml", lang=book.language,
-		content=title_template.format(
-			title=story["title"], 
-			author=story["user"]["name"], 
-			username=story["user"]["username"],
-			description=story["description"],
-			reads=story["readCount"],
-			votes=story["voteCount"],
-			comments=story["commentCount"],
-			first_publish=story["createDate"],
-			last_update=story["modifyDate"]).encode())
-
-	book.add_item(cover)
-	book.add_item(title)
-
-	toc = [cover, title]
-	for part in story["parts"]:
-		print("\tDownloading part \"" + part["title"] + "\"")
-
-		chap = epub.EpubHtml(
-				uid=str(part["id"]),
-				title=part["title"], 
-				file_name=str(part["id"]) + ".xhtml")
-
-		orig = get_request(part["text_url"]["text"]).content
-
-		# Building header
-		orig = build_chapter_header(part).encode() + orig
-
-		if b"<img" in orig:
-			soup = bs4.BeautifulSoup(orig, "html.parser")
-			for img in soup.find_all("img"):
-				url = img["src"]
-				if "img.wattpad.com" in url:
-					print(f"\t\tRelinking image \"{ (url[:51] + '...') if len(url) > 54 else url }\"")
-
-					imgname = f"images/{part['id']}/{url.split('/')[-1]}"
-					
-					book.add_item(epub.EpubItem(
-							file_name=imgname,
-							media_type="image/jpeg",
-							content=get_request(url).content))
-					img["src"] = imgname
-			chap.set_content(soup.prettify())
+		if story["language"]["id"] in LANGUAGES:
+			book.set_language(LANGUAGES[story["language"]["id"]])
 		else:
-			chap.set_content(orig)
+			print("Unknown language tag for ID " \
+				+ f"{str(story['language']['id'])} (\"{story['language']['name']}\"), " \
+				+ "defaulting to \"en\"", file=sys.stderr)
+			book.set_language("en")
 
-		toc.append(chap)
-		book.add_item(chap)
+		book.add_author(f"{story['user']['name']} ({story['user']['username']})")
+		book.set_cover("cover.jpg", get_request(story["cover"]).content)
 
-	book.toc = tuple(toc)
-	book.spine = ['nav', *toc]
+		# Buttload of metadata ('cause we've got it so why not)
+		book.add_metadata('DC', 'description', story["description"])
+		book.add_metadata('DC', 'publisher', "Wattpad")
+		book.add_metadata('DC', 'date', story["createDate"])
+		book.add_metadata('DC', 'subject', ", ".join(story["tags"]))
+		book.add_metadata('DC', 'type', "Text")
+		book.add_metadata('DC', 'format', "application/epub+zip")
+		book.add_metadata('DC', 'identifier', "wp" + story["id"])
+		book.add_metadata('DC', 'source', story["url"])
+		book.add_metadata('DC', 'language', book.language)
+		book.add_metadata('DC', 'creator', story["user"]["name"])
+		book.add_metadata('DC', 'creator', story["user"]["username"])
 
-	book.add_item(epub.EpubNav())
-	book.add_item(epub.EpubNcx())
+		cover = epub.EpubHtml(title="Cover", file_name="cover_page.xhtml", lang=book.language, 
+	cover = epub.EpubHtml(title="Cover", file_name="cover_page.xhtml", lang=book.language, 
+		cover = epub.EpubHtml(title="Cover", file_name="cover_page.xhtml", lang=book.language, 
+			content=cover_template.format(title=story["title"]).encode())
+		title = epub.EpubHtml(title="Title Page", file_name="title.xhtml", lang=book.language,
+			content=title_template.format(
+				title=story["title"], 
+			title=story["title"], 
+				title=story["title"], 
+				author=story["user"]["name"], 
+			author=story["user"]["name"], 
+				author=story["user"]["name"], 
+				username=story["user"]["username"],
+				description=story["description"],
+				reads=story["readCount"],
+				votes=story["voteCount"],
+				comments=story["commentCount"],
+				first_publish=story["createDate"],
+				last_update=story["modifyDate"]).encode())
 
-	epub.write_epub(filename, book)
+		book.add_item(cover)
+		book.add_item(title)
 
-	print(f"Saved {story['title']} by {story['user']['name']} to {filename}, took {round(time.time() - loc, 2)}s")
-	print()
+		toc = [cover, title]
+		for part in story["parts"]:
+			print("\tDownloading part \"" + part["title"] + "\"")
 
-print("Processed " + str(len(result["stories"])) + " stories, took " + str(round(time.time() - all, 2)) + "s")
+			chap = epub.EpubHtml(
+					uid=str(part["id"]),
+					title=part["title"], 
+				title=part["title"], 
+					title=part["title"], 
+					file_name=str(part["id"]) + ".xhtml")
+
+			orig = get_request(part["text_url"]["text"]).content
+
+			try:
+				err = json.loads(orig.decode())
+				print(f"Error while downloading part \"{part['title']}\": {err['message']}", file=sys.stderr)
+				errors += 1
+			except:
+				pass
+
+			# Building header
+			orig = build_chapter_header(part).encode() + orig
+
+			if b"<img" in orig:
+				soup = bs4.BeautifulSoup(orig, "html.parser")
+				for img in soup.find_all("img"):
+					url = img["src"]
+					if "img.wattpad.com" in url:
+						print(f"\t\tRelinking image \"{ (url[:51] + '...') if len(url) > 54 else url }\"")
+
+						imgname = f"images/{part['id']}/{url.split('/')[-1]}"
+						
+						book.add_item(epub.EpubItem(
+								file_name=imgname,
+								media_type="image/jpeg",
+								content=get_request(url).content))
+						img["src"] = imgname
+				chap.set_content(soup.prettify())
+			else:
+				chap.set_content(orig)
+
+			toc.append(chap)
+			book.add_item(chap)
+
+		book.toc = tuple(toc)
+		book.spine = ['nav', *toc]
+
+		book.add_item(epub.EpubNav())
+		book.add_item(epub.EpubNcx())
+
+		epub.write_epub(filename, book)
+
+		print(f"Saved {story['title']} by {story['user']['name']} to {filename}, took {round(time.time() - loc, 2)}s")
+		print()
+
+	if MULTITHREAD:
+		t = threading.Thread(target=process_story, args=(story,))
+		threads.append(t)
+		t.start()
+	else:
+		process_story(story)
+
+if MULTITHREAD:
+	print("Waiting for all threads to finish")
+
+	for t in threads:
+		t.join()
+	print("\n\nAll jobs done\n")
+
+print("Processed " + str(len(result["stories"])) + " stories with " + str(errors) + " errors, took " + str(round(time.time() - all, 2)) + "s")
